2bf40094d8a6202c71b9923520fe66dcd17a25ad
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-10 12:00:54 +0100

    Add star rating component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import TipCalculator from "./components/tipCalcuator/TipCalculator";
+import Rating from "./components/starRating/Rating";
 
 function App() {
   return (
     <div>
       <h1>Component Catalog</h1>
-      <TipCalculator />
+      <div>
+        <TipCalculator />
+      </div>
+      <div>
+        <Rating
+          maxRating={5}
+          messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+          defaultRating={3}
+        />
+      </div>
     </div>
   );
 }
